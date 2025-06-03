c913281a1c69d8836d31b0eff2b18f9a76e3f1c9
gerryvoy <gov_9@hotmail.com>
2025-06-03 16:39:02 -0600

    First Commit

---
+++ src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    title: 'Blog de prueba',
+    description: 'Este es un blog de prueba by .:][ gErRy_VoY ][:.',
+}
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,28 @@
+import rss from '@astrojs/rss';
+import type { APIRoute } from "astro";
+import { getCollection } from 'astro:content';
+
+export const GET: APIRoute = async ({ params, request, site }) => {
+    const blogPosts = await getCollection('blog');
+    
+    return rss({
+        stylesheet: '/styles/rss.xsl',
+        // `<title>` field in output xml
+        title: '.:][ gErRy_VoY ][:. Blog',
+        // `<description>` field in output xml
+        description: 'Blog de pruebas, sección 8. Cuerso AStro.',
+        // Pull in your project "site" from the endpoint context
+        // https://docs.astro.build/en/reference/api-reference/#site
+        site: site ?? '',
+        // Array of `<item>`s in output xml
+        // See "Generating items" section for examples using content collections and glob imports
+        items: blogPosts.map( ({ data, slug }) => ({
+            title: data.title,
+            pubDate: data.date,
+            description: data.description,
+            link: `/blog/${slug}`,
+        }) ),
+        // (optional) inject custom xml
+        customData: `<language>es-mx</language>`,
+    });
+};
